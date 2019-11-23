90765dbc78d864325ae5d5d32e2ce5f24cd79f3f
Greg Hynds <greg.hynds@bbc.co.uk>
2019-11-23 23:19:46 +0000

    Add helper functions for mocking Retrofit responses.

---
+++ app/src/test/java/com/allsouls/newsapp/util/RetrofitHelpers.kt
@@ -0,0 +1,29 @@
+package com.allsouls.newsapp.util
+
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Deferred
+import okhttp3.MediaType
+import okhttp3.ResponseBody
+import retrofit2.Response
+
+const val MEDIA_TYPE = "application/json; charset=UTF-8"
+
+fun <T> createSuccessResponse(body: T): Response<T> {
+    return Response.success(body)
+}
+
+fun <T> createErrorResponse(message: String, status: Int = 500): Response<T> {
+    return Response.error(status, createResponseBody(message))
+}
+
+fun createResponseBody(body: String): ResponseBody {
+    return ResponseBody.create(MediaType.parse(MEDIA_TYPE), body)
+}
+
+fun <T> successResponse(body: T): Deferred<Response<T>> {
+    return CompletableDeferred(createSuccessResponse(body))
+}
+
+fun <T> failureResponse(message: String, status: Int = 500): Deferred<Response<T>> {
+    return CompletableDeferred(createErrorResponse(message, status))
+}
