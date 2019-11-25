83765ef548d189cf338a65e21563aff3cddb6321
Greg Hynds <greg.hynds@bbc.co.uk>
2019-11-25 20:22:13 +0000

    Add headlines list to FeedActivity.

--- app/src/main/java/com/allsouls/newsapp/feed/ui/FeedActivity.kt
+++ app/src/main/java/com/allsouls/newsapp/feed/ui/FeedActivity.kt
@@ -3,11 +3,12 @@ package com.allsouls.newsapp.feed.ui
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.allsouls.newsapp.R
 import com.allsouls.newsapp.feed.domain.entity.Feed
 import com.allsouls.newsapp.feed.presentation.FeedPresenter
 import com.allsouls.newsapp.feed.presentation.FeedView
-import kotlinx.android.synthetic.main.activity_feed.button
+import kotlinx.android.synthetic.main.activity_feed.*
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 
@@ -15,11 +16,13 @@ class FeedActivity : AppCompatActivity(), FeedView {
 
     private val presenter: FeedPresenter by inject { parametersOf(this) }
 
+    private lateinit var adapter: HeadlinesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
 
-        bindActions()
+        presenter.load()
     }
 
     override fun onDestroy() {
@@ -28,18 +31,19 @@ class FeedActivity : AppCompatActivity(), FeedView {
     }
 
     override fun showFeed(feed: Feed) {
-        makeToast("Loaded feed!")
+        adapter = createAdapter(feed)
+        headlinesList.adapter = adapter
+        headlinesList.layoutManager = LinearLayoutManager(this)
     }
 
     override fun showError(error: Throwable) {
-        makeToast("Couldn't load feed.")
+        Toast.makeText(this, "Couldn't load feed.", Toast.LENGTH_SHORT).show()
     }
 
-    private fun makeToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun bindActions() {
-        button.setOnClickListener { presenter.load() }
+    private fun createAdapter(feed: Feed): HeadlinesAdapter {
+        return HeadlinesAdapter(
+            feed.headlines,
+            onItemClick = { /* no op */ }
+        )
     }
 }
