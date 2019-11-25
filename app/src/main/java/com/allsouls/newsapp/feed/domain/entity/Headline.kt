bbc1ef05797d206da29968bd1cf57a4fef1c1d5c
Greg Hynds <greg.hynds@bbc.co.uk>
2019-11-25 21:51:20 +0000

    Change headline entity to extend Parcelable.

--- app/src/main/java/com/allsouls/newsapp/feed/domain/entity/Headline.kt
+++ app/src/main/java/com/allsouls/newsapp/feed/domain/entity/Headline.kt
@@ -1,9 +1,12 @@
 package com.allsouls.newsapp.feed.domain.entity
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.util.Date
 
+@Parcelize
 data class Headline(
     val headline: String,
     val updated: Date,
     val introduction: String
-)
+) : Parcelable
