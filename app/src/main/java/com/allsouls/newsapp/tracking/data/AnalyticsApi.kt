00cbbeb8d1d41dc255de4b046f81e79877a1a83b
Greg Hynds <greg.hynds@bbc.co.uk>
2019-11-26 23:44:06 +0000

    Add logging to analytics API.

--- app/src/main/java/com/allsouls/newsapp/tracking/data/AnalyticsApi.kt
+++ app/src/main/java/com/allsouls/newsapp/tracking/data/AnalyticsApi.kt
@@ -1,5 +1,6 @@
 package com.allsouls.newsapp.tracking.data
 
+import com.allsouls.newsapp.BuildConfig
 import com.allsouls.newsapp.arch.data.toResult
 import com.allsouls.newsapp.tracking.domain.Event
 import com.allsouls.newsapp.tracking.domain.Tracker
@@ -7,6 +8,10 @@ import com.allsouls.newsapp.tracking.domain.Tracker
 class AnalyticsApi(private val client: AnalyticsClient) : Tracker {
 
     override suspend fun track(event: Event): Result<Unit> {
+        if (BuildConfig.DEBUG) {
+            println("Analytics: ${event.toMap()}")
+        }
+
         return client.track(event.toMap())
             .await()
             .toResult()
