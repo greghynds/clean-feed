07b7331a00ceee6ed5a1b866a2c86959b624b41b
Greg Hynds <greg.hynds@bbc.co.uk>
2021-08-06 00:28:28 +0100

    Add action for selecting headline

--- app/src/test/java/com/allsouls/newsapp/feed/presentation/FeedActionsTest.kt
+++ app/src/test/java/com/allsouls/newsapp/feed/presentation/FeedActionsTest.kt
@@ -1,8 +1,10 @@
 package com.allsouls.newsapp.feed.presentation
 
 import com.allsouls.newsapp.arch.presentation.Action
+import com.allsouls.newsapp.headline.domain.entity.Headline
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import java.util.*
 
 class FeedActionsTest {
 
@@ -11,6 +13,16 @@ class FeedActionsTest {
         val expected = Action("LOAD_FEED")
 
         val result = createLoadFeedAction()
+
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `creates an action for selecting a headline`() {
+        val headline = Headline("headline", Date(), "introduction")
+        val expected = Action("SELECT_HEADLINE", headline)
+
+        val result = createSelectHeadlineAction(headline)
 
         assertThat(result).isEqualTo(expected)
     }
